dd7bd4a0b44063222eac7dbeec2bd83265822e09
Hypersoft Inc <social.yaqoob@gmail.com>
2025-01-07 12:03:20 +0500

    opt

--- app/src/main/java/com/orbitalsonic/offlineprayertime/prayer/DailyPrayerTimeFragment.kt
+++ app/src/main/java/com/orbitalsonic/offlineprayertime/prayer/DailyPrayerTimeFragment.kt
@@ -51,7 +51,7 @@ class DailyPrayerTimeFragment : Fragment() {
             longitude = longitude,
             highLatitudeAdjustment = HighLatitudeAdjustment.NO_ADJUSTMENT,
             asrJuristicMethod = AsrJuristicMethod.HANAFI,
-            prayerTimeConvention = PrayerTimeConvention.KARACHI,
+            prayerTimeConvention = PrayerTimeConvention.MAKKAH,
             timeFormat = TimeFormat.HOUR_12
         ) { result ->
             result.onSuccess { prayerItem ->
